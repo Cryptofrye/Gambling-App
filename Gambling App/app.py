7f36ea8c6f9e3653d1dde4fd9236fdcb1cd26818
Owen Throup <owenthroupy@gmail.com>
2019-10-11 10:39:05 +0100

    Updated User Model with passwords
    Update the user model to implement passwords. The request route now works and inserts the user to the database when a valid POST request with form data is sent. Next thing to do is hashing and probably salting the passwords.

--- Gambling App/app.py
+++ Gambling App/app.py
@@ -1,13 +1,26 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask import Flask, request
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///GamblingApp.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 @app.route("/ping")
 def ping():
     return "Pong"
+
+@app.route("/register", methods=["POST"])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = models.User(username=username, money=5.00, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return f"User Inserted - {username} : {password}"
+    return "403 - Method not allowed"
 
 import models
 
--- Gambling App/models.py
+++ Gambling App/models.py
@@ -4,6 +4,7 @@ class User(app.db.Model):
     id = app.db.Column(app.db.Integer, primary_key=True)
     username = app.db.Column(app.db.String(80), unique=True, nullable=False)
     money = app.db.Column(app.db.Float)
+    password = app.db.Column(app.db.String(80), unique=False, nullable=False)
 
     def __repr__(self):
         return f"<User {self.username} - £{self.money}"
