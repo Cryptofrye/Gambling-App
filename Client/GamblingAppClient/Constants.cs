1ac1c8a5e0bb08281f943e0d80dbe2b1e5af4565
Ben Chadwick <32690103+BurnoutBen@users.noreply.github.com>
2019-10-11 18:50:33 +0100

    Create Constants.cs

---
+++ Client/GamblingAppClient/Constants.cs
@@ -0,0 +1,9 @@
+namespace GamblingAppClient
+{
+    class Constants
+    {
+        static readonly string serverIP = "XXX.XXX.XXX.XXX"; //Replace this with the IP of your server running the RESTful API
+        static readonly int serverPort = 1337; //Replace this with your port. 1337 is the default
+        static readonly string registerUserEndpoint = "/register"; //The URL endpoint that will be used to register users with. /register is default
+    }
+}
