470aa54a8ed958c532de39ebb42cb47a41587946
Owen Throup <38217027+Throupy@users.noreply.github.com>
2019-10-15 16:47:12 +0100

    Added flask-migrate

--- Server/server/__init__.py
+++ Server/server/__init__.py
@@ -4,12 +4,14 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, current_user
 from flask_admin import Admin, BaseView, expose
 from flask_admin.contrib.sqla import ModelView
+from flask_migrate import Migrate, MigrateCommand  
 from server.config import Config
 
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
+migrate = Migrate()
 app = Flask(__name__)
 
 def create_app():
@@ -19,9 +21,10 @@ def create_app():
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
+    migrate.init_app(app, db)
     from server.models import User
     from server.adminsite.admin_routes import AdminLogin, AdminModelView, CustomAdminIndexView
-    admin = Admin(app, name='gambling app', template_mode='bootstrap3', index_view=CustomAdminIndexView())
+    admin = Admin(app, name="Gambling Application Administrator's Panel", template_mode='bootstrap3', index_view=CustomAdminIndexView())
     admin.add_view(AdminModelView(User, db.session))
     admin.add_view(AdminLogin(endpoint="login"))
     from server.main.routes import main
