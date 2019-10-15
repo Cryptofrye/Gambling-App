d6fe98926868186b09eab71ea732238c6258fcd1
Owen Throup <owenthroupy@gmail.com>
2019-10-15 12:49:28 +0100

    Username bounds and security
    Username must be between 4 and 16 chars, password must be between 6 and 42. Also you have to pass your old password in order to update your password.

--- Server/server/users/routes.py
+++ Server/server/users/routes.py
@@ -11,8 +11,8 @@ def register():
         # Get username and password from the POST request.
         username = request.form.get('username')
         password = request.form.get('password')
-        if len(username) < 4 or len(password) < 6:
-            return abort(403, "Username must be at least 4 characters, and password must be at least 6.")
+        if not (len(username) > 4 and len(username) < 16) or not (len(password) > 6 and len(password) < 42):
+            return abort(403, "Username must be between 4 and 16 characters, and password must be between 6 and 42.")
         hashed_password = bcrypt.generate_password_hash(password).decode("UTF-8")
         isadmin = False
         if username.lower() == 'throupy' or username.lower() == 'chadders':
@@ -66,22 +66,23 @@ def user(username):
         )
     elif request.method == "POST":
         if current_user == user:
-            old_name = user.username
+            oldUserInstance = user
             returnMessage = ""
             # request.form.get returns None if no parameter is found, rather than raising an error.
-            if not request.form.get("username") and not request.form.get("password"):
-                return abort(500, "Expected a value to change")
+            if not request.form.get("oldPassword") or\
+                not bcrypt.check_password_hash(oldUserInstance.password, str(request.form.get("oldPassword"))):
+                return abort(500, "Parameter 'oldpassword' was incorrect or missing")
             if request.form.get("username"):
                 # After requst.form.get() I know the username exists, so i can use request.form["username"]
                 if len(request.form["username"]) > 4 and request.form["username"] != user.username:
                     user.username = request.form["username"]
                     db.session.commit()
-                    returnMessage += f"Credentials Updated for {old_name}. Username changed to {request.form['username']}\n"
-            if request.form.get("password"):
-                if len(request.form["password"]) > 6:
-                    user.password = bcrypt.generate_password_hash(request.form["password"]).decode("UTF-8")
+                    returnMessage += f"Credentials Updated for {oldUserInstance.username}. Username changed to {request.form['username']}\n"
+            if request.form.get("newPassword"):
+                if len(request.form["newPassword"]) > 6:
+                    user.password = bcrypt.generate_password_hash(request.form["newPassword"]).decode("UTF-8")
                     db.session.commit()
-                    returnMessage += f"Credentials Updated for {old_name}. Password Changed"
+                    returnMessage += f"Credentials Updated for {oldUserInstance.username}. Password Changed"
             return returnMessage
         else:
             return abort(403, "You must log in to change your credentials")
